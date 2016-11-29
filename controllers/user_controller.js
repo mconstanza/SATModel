7f096eac715ea55461b70766a57249e8e06bae78
Joshua Hudson <joshudson93@gmail.com>
2016-11-28 21:03:21 -0500

    Added get method for pulling test per student. Messy but functional

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -14,13 +14,34 @@ var SAT = require('../js/sat_scoring.js');
 var router = express.Router();
 
 // DEVELOPMENT -- remove in final product
+// Grabbing our users along with the tests they take
 router.get('/users', function(req, res) {
-    models.User.findAll()
+    models.User.findAll({
+        include: [models.StudentAnswer]
+    })
         .then(function(users) {
-            console.log(users);
+            for(var i = 0; i < users.length; i++){
+                console.log(users[i]);
+            }
+            console.log(StudentAnswer);
+            // console.log(users);
             res.send(users);
         });
 });
+
+// If we are passing in a request to lookup the user then we use this method.
+// router.get('/api/:student?', function(req, res) {
+//     // var thisUser = req.params.users;
+//     models.User.findAll({
+//         include: [models.StudentAnswer]
+//     })
+//         .then(function(student) {
+//             var user = req.params.student;
+//             console.log(StudentAnswer);
+//             console.log(student);
+//             res.send(student);
+//         });
+// });
 //=============================================
 // USER PROFILE
 //=============================================
