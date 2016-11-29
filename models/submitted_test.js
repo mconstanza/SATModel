48fa4ab4e293949c07069c716b842891174f3720
Mike Constanza <michael.constanza@gmail.com>
2016-11-28 20:54:53 -0500

    added all scaled scores to submitted test model and updated database columns to match.

--- models/submitted_test.js
+++ models/submitted_test.js
@@ -10,7 +10,17 @@ module.exports = function(sequelize, DataTypes) {
     writingTest: DataTypes.INTEGER,
     mathTest: DataTypes.DECIMAL,
     readingScaled: DataTypes.INTEGER,
-    mathScaled: DataTypes.INTEGER
+    mathScaled: DataTypes.INTEGER,
+    expressionOfIdeas: DataTypes.INTEGER,
+    standardEnglishConventions: DataTypes.INTEGER,
+    heartOfAlgebra: DataTypes.INTEGER,
+    problemSolvingDataAnalysis: DataTypes.INTEGER,
+    passportToAdvMath: DataTypes.INTEGER,
+    wordsInContext: DataTypes.INTEGER,
+    commandOfEvidence: DataTypes.INTEGER,
+    history: DataTypes.INTEGER,
+    science: DataTypes.INTEGER
+
   }, {
     classMethods: {
       associate: function(models) {
